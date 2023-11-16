08c61bef4963ef131d5b9ec9a6785c002fef58e3
Arsh Gogia <arshgogia20@gmail.com>
2023-11-16 10:11:06 +0400

    feat: all input cells added using js

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
-let ans = "";
+$(document).ready(function () {
 
-let n = 52;
+    let CellContainer = $(".input-cell-container");
 
-while (n > 0) {
-    let rem = (n - 1) % 26;
-    ans = String.fromCharCode(rem + 65) + ans;
-    n = Math.floor((n - 1) / 26);
+    for (let i = 1; i <= 50; i++) {
+        let ans = "";
+
+        let n = i;
+
+        while (n > 0) {
+            let rem = (n - 1) % 26;
+            ans = String.fromCharCode(rem + 65) + ans;
+            n = Math.floor((n - 1) / 26);
+        }
+
+        let column = $(`<div class="column-name" id="colCod-${ans} colId- ${i}">${ans}</div>`);
+        $(".column-name-container").append(column);
+        let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+        $(".row-name-container").append(row);
+
+    }
+
+     for(let i = 1; i<=50; i++){
+        let cell = $(`<div class="cell-row"></div>`)
+        for(let j = 1; j<=50; j++){
+                let cellrow = $(`<div class="input-cell" contenteditable="true"></div>`);
+                cell.append(cellrow);
+            }
+    
+            CellContainer.append(cell);
+        } 
+    
+
+});
+
+
+
+
+/*let ans = "";
+
+let n = 27;
+
+while(n > 0){
+    let rem = n % 26;
+    if(rem == 0){
+        ans = "Z" + ans;
+        n = Math.floor(n/26) - 1;
+    }
+    else{
+        ans = String.fromCharCode((rem-1)+26) + ans;
+        n = Math.floor(n/26);
+    }
 }
 
 console.log(ans);
+*/
